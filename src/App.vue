b61eec7789a85e49705fb74e49f5154c9379abf2
Jaehong Hwang <dev.jaehong@gmail.com>
2021-12-28 21:27:15 +0900

    basic style set up

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,54 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style>
+@font-face {
+  font-family: 'noto-sans-black';
+  src: url('./assets/fonts/Noto_Sans_KR/NotoSansKR-Black.otf');
+}
+
+@font-face {
+  font-family: 'noto-sans-bold';
+  src: url('./assets/fonts/Noto_Sans_KR/NotoSansKR-Bold.otf');
+}
+
+@font-face {
+  font-family: 'noto-sans-medium';
+  src: url('./assets/fonts/Noto_Sans_KR/NotoSansKR-Medium.otf');
+}
+
+@font-face {
+  font-family: 'noto-sans';
+  src: url('./assets/fonts/Noto_Sans_KR/NotoSansKR-Regular.otf');
+}
+
+@font-face {
+  font-family: 'noto-sans-light';
+  src: url('./assets/fonts/Noto_Sans_KR/NotoSansKR-Light.otf');
+}
+
+@font-face {
+  font-family: 'noto-sans-thin';
+  src: url('./assets/fonts/Noto_Sans_KR/NotoSansKR-Thin.otf');
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html, body {
+  width: 100%;
+  height: 100%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'noto-sans';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000;
+  background: #fafafa;
+  width: 100%;
+  height: 100%;
 }
 </style>
