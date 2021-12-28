b0f014b125e67595c4705f3ca1ff7442d3cccc39
Jaehong Hwang <dev.jaehong@gmail.com>
2021-12-28 21:28:30 +0900

    alias setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: process.env.ELECTRON=="true" ? './' : ".",
-  plugins: [vue()]
+  plugins: [vue()],
+  alias: [
+    {find: "@", replacement: path.resolve(__dirname, 'src')}
+  ]
 })
