20eb95c1bb20aceb4c62b41acb8e1843da4b47eb
Jaehong Hwang <dev.jaehong@gmail.com>
2021-12-28 21:27:41 +0900

    add base components

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <span>hanberger</span>
+    <Logo />
+    <span>add</span>
+  </header>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+import Logo from './Logo.vue'
+
+export default defineComponent({
+  setup () {
+    const count = ref(0)
+
+    return {
+      count,
+    }
+  },
+  components: {
+    Logo,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  padding: 28px 30px 0;
+  height: 43px;
+  align-content: center;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>.todo</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+
+<style scoped>
+h1 {
+  font-family: 'noto-sans-bold';
+  font-size: 28px;
+  line-height: 28px;
+}
+</style>
