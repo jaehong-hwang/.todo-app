00b78d5ae5c3c8188692e62c2324081cda515259
Jaehong Hwang <dev.jaehong@gmail.com>
2022-02-01 22:40:57 +0900

    컬러 항목 정리

--- src/components/organisms/Menu.vue
+++ src/components/organisms/Menu.vue
@@ -35,16 +35,12 @@ export default defineComponent({
 <style lang="scss" scoped>
 .menu {
   display: block;
-  background: var(--black);
+  background: var(--dark);
   padding: 90px 0 20px 42px;
   width: 343px;
   filter: drop-shadow(4px 0px 15px rgba(0, 0, 0, 0.25));
   backdrop-filter: blur(15px);
   text-align: left;
   overflow: hidden;
-
-  @media (prefers-color-scheme: dark) {
-    background: var(--light-black);
-  }
 }
 </style>vsc 
