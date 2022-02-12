3d9f6523dae83beeb2c97baaa84a43c044e09e75
Jaehong Hwang <dev.jaehong@gmail.com>
2022-02-12 13:36:30 +0900

    (#15) 버튼에 로딩 추가
    ################
    # <type>(<scope>) : <subject>
    # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 X
    # 타입과 제목은 필수 / 범위는 선택
    # 예) feat : 로그인 기능 추가
    
    # 바로 아래 공백 유지 (제목과 본문의 분리를 위함)
    
    ################
    # Body Message (선택사항)
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    # Issue Tracker Number or URL
    
    # --- COMMIT END ---
    # <type> list
    #   feat    : 새로운 기능
    #   fix     : 버그
    #   refactor: 코드 리팩토링
    #   style   : 코드 의미에 영향을 주지 않는 변경사항 (형식 지정, 세미콜론 누락 등)
    #   docs    : 문서의 추가, 수정, 삭제
    #   test    : 테스트 추가, 수정, 삭제 (비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 부분 혹은 패키지 매니저 수정사항)
    # ------------------
    # Remember me ~
    #   Capitalize the subject line
    #     제목 첫 글자를 대문자로
    #   Use the imperative mood in the subject line
    #     제목은 명령문 사용 (과거형 X)
    #   Do not end the subject line with a period
    #     제목 끝에 마침표(.) 금지
    #   Separate subject from body with a blank line
    #     제목과 본문을 한 줄 띄워 분리하기 (빈 행으로 구분)
    #   Use the body to explain what and why vs. how
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #   Can use multiple lines with "-" for bullet points in body
    #     본문에 여러줄의 메시지(목록)를 작성할 땐 "-"로 구분
    # ------------------

--- src/components/atoms/Button/NormalButton.vue
+++ src/components/atoms/Button/NormalButton.vue
@@ -1,5 +1,6 @@
 <template>
-  <button :type="type" :class="{ primary, error }">
+  <button :type="type" :class="{ primary, error, loading }">
+    <fa-icon v-if="loading" icon="spinner" class="loading-icon" />
     <slot/>
   </button>
 </template>
@@ -11,6 +12,7 @@ export default defineComponent({
   props: {
     primary: Boolean,
     error: Boolean,
+    loading: Boolean,
     type: {
       type: String as PropType<ButtonHTMLAttributes['type']>,
       default: () => {
@@ -42,7 +44,29 @@ button {
     background: var(--error);
   }
 
-  &:hover {
+  &.loading {
+    opacity: 0.7;
+    cursor: default;
+
+    & .loading-icon {
+      margin-right: 8px;
+      animation-name: rotate;
+      animation-duration: 2s;
+      animation-iteration-count: infinite;
+    }
+
+    @keyframes rotate {
+      from {
+        transform: rotate(0deg);
+      }
+
+      to {
+        transform: rotate(360deg);
+      }
+    }
+  }
+
+  &:not(.loading):hover {
     box-shadow: 0px 5px 13px rgba(0,0,0,0.3);
   }
 }
